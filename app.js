829d1d01c40fe2a30fd01cdbace53f956e8b196d
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-07-22 13:52:18 +0200

    Added ids to inputs and removed redundant canvas code

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-function draw() {
-    const canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
+const amountInput = document.getElementById('amountInput')
+const dateInput = document.getElementById('dateInput')
+const saveBtn = document.getElementById('saveBtn')
 
-        const img = new Image();
-        img.addEventListener('load', function () {
-            ctx.drawImage(img, 0, 0);
-        })
-        img.src = "images/bon.png";
-    }
+function draw() {
+    const img = new Image();
+    img.addEventListener('load', function () {
+        ctx.drawImage(img, 0, 0);
+        //Font size for the amount
+        ctx.font = '55px gabriola'
+        ctx.fillText("50", 450, 300)
+
+        //Font size for the date
+        ctx.font = '40px gabriola'
+        ctx.fillText("05.12.2022", 410, 361)
+    })
+    img.src = "images/bon.png";
 }
 
